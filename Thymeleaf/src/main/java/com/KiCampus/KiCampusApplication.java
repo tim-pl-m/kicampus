4defd53c8911e010a59e8e6ec774749ff68baf96
Tim <tim.wieschalla@adesso.de>
2021-06-04 13:49:38 +0200

    start.spring.io generation

---
+++ Thymeleaf/src/main/java/com/KiCampus/KiCampusApplication.java
@@ -0,0 +1,13 @@
+package com.KiCampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiCampusApplication.class, args);
+	}
+
+}
---
+++ Thymeleaf/src/test/java/com/KiCampus/KiCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KiCampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
